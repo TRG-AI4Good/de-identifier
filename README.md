28f88ac2ac694280016e353ec9c11ab884cdcd07
Mohammad Askari <mhmd.askari@hotmail.com>
2023-08-11 19:51:32 -0700

    Some modifications.

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# de-identifier
-This package includes a Python class for removing Personal Identifiable Information (PII) from a given CSV file via two steps of scrubbing.
+# De-identifier
+This package includes a Python class for removing Personally Identifiable Information (PII) from a given CSV file using two levels of scrubbing.
 
 - The analyzer and anonymizer are defined based on [Microsoft Presidio](https://github.com/microsoft/presidio).
-- Two Named entity recognitions (NER) are used including:
-  1. The existing [spaCy](https://spacy.io/api/entityrecognizer) NER within Presidio package.
+- Two Named Entity Recognition (NER) methods are used, including:
+  1. The existing [spaCy](https://spacy.io/api/entityrecognizer) NER within the Presidio package.
   2. [Flair](https://github.com/flairNLP/flair) NER.
 
-- The final output is itself a CSV file. You may see the example Jupyter Notebook for reference.
+- The `CSVDeidentifier` class is a modified version of the [sample code](https://github.com/microsoft/presidio/blob/main/docs/samples/python/process_csv_file.py) provided by Presidio. The constructor of the class accepts the following arguments:
+
+  1. `input_csv_path`: The path to the input CSV file.
+  2. `output_csv_path`: The path to the output CSV file.
+  3. `language`: Supported languages are 'en', 'es', 'de', 'nl'.
+  4. `entities` (optional): A list of entities.
+  5. `add_flair` (optional): A boolean indicating whether to use Flair for NER or not.
+  6. `lower_case` (optional): A boolean indicating whether to convert words to lowercase before analysis or not.
+
+- The  `FlairRecognizer` class is based on the [sample code](https://github.com/microsoft/presidio/blob/main/docs/samples/python/flair_recognizer.py) provided by Presidio without much modification.
+- The final output is a CSV file. You can refer to the example Jupyter Notebook for guidance.
--- flair_recognizer.py
+++ flair_recognizer.py
@@ -1,13 +1,12 @@
+import logging
+from typing import Optional, List, Tuple, Set
+
 from presidio_analyzer import (
-    BatchAnalyzerEngine,
-    DictAnalyzerResult,
     RecognizerResult,
     EntityRecognizer,
     AnalysisExplanation,
-    AnalyzerEngine,
-    RecognizerRegistry)
+)
 from presidio_analyzer.nlp_engine import NlpArtifacts
-from presidio_anonymizer import BatchAnonymizerEngine
 
 try:
     from flair.data import Sentence
@@ -15,83 +14,10 @@ try:
 except ImportError:
     print("Flair is not installed")
 
-from typing import List, Dict, Iterable, Optional, Tuple, Set
-import csv
+
+logger = logging.getLogger("presidio-analyzer")
 
 
-class CSVDeidentifier(BatchAnalyzerEngine, BatchAnonymizerEngine):
-
-    def __init__(self,
-                 input_csv_path: str,
-                 output_csv_path: str,
-                 language: str,
-                 entities: List[str] = None,
-                 add_flair= False,
-                 lower_case= False, *args, **kwargs):
-
-        if add_flair:
-            flair_recognizer = (
-            FlairRecognizer()
-            )  # This would download a very large (+2GB) model on the first run
-
-            registry = RecognizerRegistry()
-            registry.add_recognizer(flair_recognizer)
-            analyzer_spaCy_flair = AnalyzerEngine(registry=registry)
-
-            BatchAnalyzerEngine.__init__(self, analyzer_engine=analyzer_spaCy_flair)
-        else:
-            BatchAnalyzerEngine.__init__(self)
-
-        BatchAnonymizerEngine.__init__(self)
-        self.input_csv_path = input_csv_path
-        self.output_csv_path = output_csv_path
-        self.language = language
-        self.lower_case = lower_case
-        self.entities = entities
-
-    def analyze_csv(
-        self,
-        keys_to_skip: Optional[List[str]] = None
-    ) -> Iterable[DictAnalyzerResult]:
-
-        with open(self.input_csv_path, 'r') as csv_file:
-            csv_list = list(csv.reader(csv_file))
-            if self.lower_case:
-                csv_list = [[element.lower() if "<" not in element else element for element in csv_row] for csv_row in csv_list]
-            
-            csv_dict = {header: list(map(str, values)) for header, *values in zip(*csv_list)}
-            analyzer_results = self.analyze_dict(csv_dict, self.language, keys_to_skip, entities = self.entities)
-            return list(analyzer_results)
-        
-    def anonymize_csv(
-            self,
-            **kwargs) -> Dict[str, str]:
-        anonymizer_results = self.anonymize_dict(self.analyze_csv(), **kwargs)
-        return anonymizer_results
-
-
-    def write_output(
-            self) -> None:
-        
-        anonymizer_results = self.anonymize_csv()
-
-        # Get the headers
-        headers = list(anonymizer_results.keys())
-
-        # Convert the dictionary to a list of dictionaries (rows)
-        rows = [dict(zip(headers, row)) for row in zip(*anonymizer_results.values())]
-
-        with open(self.output_csv_path, 'w') as csv_output_file:
-            writer = csv.DictWriter(csv_output_file, headers)
-            writer.writeheader()  # Writing headers
-            writer.writerows(rows)
-            
-    def run(self) -> None:
-        self.write_output()
-
-
-
-        
 class FlairRecognizer(EntityRecognizer):
     """
     Wrapper for a flair model, if needed to be used within Presidio Analyzer.
